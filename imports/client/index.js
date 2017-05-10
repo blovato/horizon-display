e0e4b03820965a84f194a04197cb2dc950d7e299
Tyler Fields <tyler.fields777@gmail.com>
2017-05-10 13:56:53 -0700

    edit to circles that will be changed

--- imports/client/index.js
+++ imports/client/index.js
@@ -6,7 +6,7 @@ class MainPage extends React.Component {
   constructor() {
     super();
 
-    this.state = {rect1: '', rect2: '', rect3: '', rect4: ''};
+    this.state = {rect1: '', rect2: '', cir1: '', cir2: ''};
   }
 
   componentDidMount() {
@@ -22,68 +22,68 @@ class MainPage extends React.Component {
     const date = new Date;
     const hour = date.getHours();
     const min = date.getMinutes();
-
     if (hour === 6 && (min > 0 && min < 6) ||
         (initialize === 'init' && (hour >= 6 && hour< 8))) {
       this.setState({
         rect1: 'transition1-1',
         rect2: 'transition1-2',
-        rect3: 'transition1-3',
-        rect4: 'transition1-4'});
-      setTimeout(() => this.timeOfDayColorChange(hour), 30000);
+        cir1: 'transition1-3',
+        cir2: 'transition1-4'});
+      setTimeout(() => this.timeOfDayColorChange(hour), 15000);
     } else if (hour === 8 && (min > 0 && min < 6) ||
         (initialize === 'init' && (hour >= 8 && hour< 20))) {
       this.setState({
         rect1: 'transition2-1',
         rect2: 'transition2-2',
-        rect3: 'transition2-3',
-        rect4: 'transition2-4'});
-      setTimeout(() => this.timeOfDayColorChange(hour), 30000);
+        cir1: 'transition2-3',
+        cir2: 'transition2-4'});
+      setTimeout(() => this.timeOfDayColorChange(hour), 15000);
     } else if ((hour === 20 && (min > 0 && min < 6)) ||
         (initialize === 'init' && (hour >= 20 && hour< 22))) {
           console.log('start');
       this.setState({
         rect1: 'transition3-1',
         rect2: 'transition3-2',
-        rect3: 'transition3-3',
-        rect4: 'transition3-4'});
-      setTimeout(() => this.timeOfDayColorChange(hour), 30000);
+        cir1: 'transition3-3',
+        cir2: 'transition3-4'});
+      setTimeout(() => this.timeOfDayColorChange(hour), 15000);
     } else if ((hour === 22 && (min > 0) && (min < 6)) ||
         (initialize === 'init' && (hour >= 22 || hour < 6))) {
       this.setState({
         rect1: 'transition4-1',
         rect2: 'transition4-2',
-        rect3: 'transition4-3',
-        rect4: 'transition4-4'});
+        cir1: 'transition4-3',
+        cir2: 'transition4-4'});
       setTimeout(() => this.timeOfDayColorChange(hour), 15000);
     }
   }
 
   timeOfDayColorChange(hour) {
-    if (hour >= 6 && hour< 8) {
+    // if (hour >= 6 && hour< 8) {
+    if (true) {
       this.setState({
         rect1: 'sunrise1',
         rect2: 'sunrise2',
-        rect3: 'sunrise3',
-        rect4: 'sunrise4'});
+        cir1: 'sunrise3',
+        cir2: 'sunrise4'});
     } else if (hour >= 8 && hour< 20) {
       this.setState({
         rect1: 'daytime1',
         rect2: 'daytime2',
-        rect3: 'daytime3',
-        rect4: 'daytime4'});
+        cir1: 'daytime3',
+        cir2: 'daytime4'});
     } else if (hour >= 20 && hour< 22) {
       this.setState({
         rect1: 'dusk1',
         rect2: 'dusk2',
-        rect3: 'dusk3',
-        rect4: 'dusk4'});
+        cir1: 'dusk3',
+        cir2: 'dusk4'});
     } else if (hour >= 22 || hour < 6) {
       this.setState({
         rect1: 'night1',
         rect2: 'night2',
-        rect3: 'night3',
-        rect4: 'night4'});
+        cir1: 'night3',
+        cir2: 'night4'});
     }
   }
 
@@ -91,10 +91,10 @@ class MainPage extends React.Component {
     return (
       <div className={classNames('Home', 'foo', 'bar')} >
         <svg>
-          <rect className={classNames('rect', this.state.rect1)} x='0%' y='-50%' width='100%' height='85%'/>
-          <rect className={classNames('rect', this.state.rect2)} x='0' y='35%' width='100%' height='25%'/>
-          <rect className={classNames('rect', this.state.rect3)} x='0' y='60%' width='100%' height='25%'/>
-          <rect className={classNames('rect', this.state.rect4)} x='0' y='85%' width='100%' height='50%'/>
+          <rect className={classNames('rect', this.state.rect1)} x='0%' y='-50%' width='100%' height='95%'/>
+          <rect className={classNames('rect', this.state.rect2)} x='0' y='45%' width='100%' height='85%'/>
+          <circle className={classNames('circle', this.state.cir1)} cx="50%" cy="125%" r="40%" />
+          <circle className={classNames('circle', this.state.cir2)} cx="50%" cy="125%" r='25%' />
         </svg>
       </div>
     );
