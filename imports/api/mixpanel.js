dbd0d93a6fd1ef9b8b8b9ce7acb54a4d8b30fc1c
Tyler Fields <tyler.fields777@gmail.com>
2017-08-09 13:18:47 -0700

    add logs on server side

--- imports/api/mixpanel.js
+++ imports/api/mixpanel.js
@@ -11,11 +11,16 @@ export function scrapeUserCountFromAdmin() {
     .authentication(...ADMIN_AUTH.split(':'))
     .goto('http://ad.shop.co')
     .evaluate(() => {
+      console.log('eval1');
       return window.Meteor.subscribe('dashboard.users.countTotal');
     })
     .wait(1000) // wait for sub to load
     .evaluate(() => {
+      console.log('eval2');
       return window.Counter.get('dashboard.users.countTotal');
+    })
+    .catch(function(err) {
+      console.log('in the catch', err.message);
     });
 }
 
@@ -26,11 +31,16 @@ export function scrapeJobCountFromAdmin() {
     .authentication(...ADMIN_AUTH.split(':'))
     .goto('https://parachute.shop.co')
     .evaluate(() => {
+      console.log('eval1');
       return window.Meteor.subscribe('tasks.count.new');
     })
     .wait(1000) // wait for sub to load
     .evaluate(() => {
+      console.log('eval2');
       return window.Counts.get('tasks.count.new');
+    })
+    .catch(function(err) {
+      console.log('in the catch', err.message);
     });
 }
 
