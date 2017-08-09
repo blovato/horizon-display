b8f0ecc682457328bd1e141b9dbb0a4b494b9b9a
Tyler Fields <tyler.fields777@gmail.com>
2017-08-09 15:31:11 -0700

    add log to try and track

--- imports/api/mixpanel.js
+++ imports/api/mixpanel.js
@@ -7,6 +7,7 @@ const { ADMIN_AUTH } = process.env;
  * @return {Promise}
  */
 export function scrapeUserCountFromAdmin() {
+  console.log('trying to scrape');
   return Nightmare({ maxAuthRetries: 3 })
     .authentication(...ADMIN_AUTH.split(':'))
     .goto('http://ad.shop.co')
